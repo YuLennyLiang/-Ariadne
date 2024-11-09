b996b0cbc852818e447ac03b57f4e74798c8f462
YU LIANG <liangyu@kratos18.ethz.ch>
2024-11-10 00:23:09 +0100

    get coverage and accuracy

--- AppTrace/earth/collect_result.sh
+++ AppTrace/earth/collect_result.sh
@@ -1,7 +1,7 @@
 root_path=$(pwd)
 trace_paths=(
     ./round1/hot/
-    ./round1/cold/
+    # ./round1/cold/
 )
 script_paths=(
     ./scripts
@@ -10,9 +10,9 @@ script_paths=(
 for trace_path in ${trace_paths[@]};
 do
     cd $root_path/$trace_path
-    echo "Current path: $(pwd)"
+    # echo "Current path: $(pwd)"
     for script_path in ${script_paths[@]};
     do
-        python3 $root_path/$script_path/get_coverage.py
+        python3 $root_path/$script_path/get_cov_acc.py
     done
 done
